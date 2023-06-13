6535a8b710d86a14efa892a9ad1ce9810f03dd83
EUNJI <eunji9180@naver.com>
2023-06-13 20:02:43 +0900

    230613_Eunji_feat: 메인페이지 Header, SignIn, SignUp, SignOut 추가

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
-
-const Title = styled.h1`
-  font-size: 3rem;
-`;
+import Header from 'Components/Header/Header';
 
 const Home = () => {
-  return <Title>OP-AL 프로젝트 임시 Home페이지 입니다.</Title>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 };
 
 export default Home;
--- src/Pages/Routes.tsx
+++ src/Pages/Routes.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Home/Home';
+import SignUp from './SignUp/SignUp';
+import SignIn from './SignIn/SignIn';
 
 const RoutesComponent = () => {
   return (
     <Routes>
       <Route path="/*" element={<Home />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/signin" element={<SignIn />} />
     </Routes>
   );
 };
