d673488a0531e182fc1e238a680b1c96735b4a63
JuHarim <wngkfla01@gmail.com>
2023-06-14 20:03:59 +0900

    230614_Harim_feat: MyPage 탭 추가, Redux설명주석 및 tab 추가

--- src/redux/reducer/reducer.tsx
+++ src/redux/reducer/reducer.tsx
@@ -1,3 +1,42 @@
-export const reducer = () => {
-  return 0;
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+// 코드 흐름을 따라가면서 이해해보시고 추가로 작성해주시면 됩니다.
+
+// 타입을 정의합니다.
+interface TabState {
+  selectedTab: string;
+}
+
+// 초기값 설정입니다.
+const initialState: TabState = {
+  selectedTab: '내 정보',
+};
+
+// createSlice 함수는 리듀서 함수와 해당 리듀서의 액션 생성자 함수들을 한 번에 생성해주는 편리한 기능을 제공합니다.
+// createSlice를 호출하면 반환되는 객체에는 reducer와 actions라는 두 가지 속성이 있습니다.
+// - reducer: 생성된 리듀서 함수입니다. 이 리듀서 함수는 tabSlice.reducer로서 store의 리듀서로 사용됩니다.
+// - actions: 생성된 액션 생성자 함수들이 포함된 객체입니다. 액션 생성자 함수들은 해당 slice의 액션을 생성하는데 사용됩니다.
+const tabSlice = createSlice({
+  name: 'tab',
+  initialState,
+  reducers: {
+    selectTab: (state, action: PayloadAction<string>) => {
+      state.selectedTab = action.payload;
+    },
+  },
+});
+
+// 모든 리듀서의 상태 타입을 포함하여 묶어서 내보내는 역할을 합니다.
+export interface RootState {
+  // 상태 타입을 추가해주세요
+  tabSlice: TabState;
+}
+
+// createSlice 함수에서 반환된 action을 내보냅니다.
+// 다른 action들도 아래와 같이 추가로 작성하셔서 내보내면 됩니다.
+export const { selectTab } = tabSlice.actions;
+
+// createSlice 함수에서 반환된 reducer를 내보냅니다.
+export default {
+  // 다른 reducer들도 아래와 같이 추가로 작성하셔서 내보내면 됩니다.
+  tabSlice: tabSlice.reducer,
 };
--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,3 +1,9 @@
-export const store = () => {
-  return 0;
-};
+import { configureStore } from '@reduxjs/toolkit';
+import reducers from './reducer/reducer';
+
+// reducer.tsx 파일에서 내보내진 reducer들을 configureStore에 전달하여 하나로 결합합니다.
+const store = configureStore({
+  reducer: reducers,
+});
+
+export default store;
