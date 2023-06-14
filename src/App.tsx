90d2759e7e6e0730b9ea349deb7aba0249c4c939
EUNJI <eunji9180@naver.com>
2023-06-14 20:13:50 +0900

    230614_Eunji_feat: header, footer 공통영역으로 고정

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import RoutesComponent from './Pages/Routes';
 import Globalstyle from './Styles/Globalstyle';
+import Header from 'Components/Header/Header';
+import Footer from 'Components/Footer/Footer';
 
 function App(): JSX.Element {
   return (
     <>
       <Globalstyle />
       <Router>
+        <Header />
         <RoutesComponent />
+        <Footer />
       </Router>
     </>
   );
---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Footer = () => {
+  return <div>이건 푸터닷</div>;
+};
+
+export default Footer;
