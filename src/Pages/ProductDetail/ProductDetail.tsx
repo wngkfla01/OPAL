9e5829aa18c013ffbc7b59b777f150cb988f898a
Geumyeop Yoon <yoongy3333@gmail.com>
2023-06-28 13:15:56 +0900

    230628_Geumyeop_Feat. 예약 관련 옵션 reducer 추가

--- src/Pages/ProductDetail/ProductDetail.tsx
+++ src/Pages/ProductDetail/ProductDetail.tsx
@@ -8,13 +8,22 @@ import {
   TimePicker,
   Modal,
   ConfigProvider,
+  InputNumber,
+  DatePickerProps,
 } from 'antd';
 import { productDetailApi } from 'api';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { RootState, updateProductDetail } from 'redux/reducer/reducer';
+import {
+  RootState,
+  updateProductDetail,
+  selectedDate,
+  selectedTime,
+  selectedGuests,
+} from 'redux/reducer/reducer';
 import { useCookies } from 'react-cookie';
 import styles from 'Styles/ProductDetail.module.scss';
+import type { Dayjs } from 'dayjs';
 import dayjs from 'dayjs';
 import 'dayjs/locale/ko';
 import locale from 'antd/locale/ko_KR';
@@ -22,26 +31,21 @@ import locale from 'antd/locale/ko_KR';
 // dayjs 라이브러리 한글화
 dayjs.locale('ko');
 
-// 제품의 상세 내용
-interface ResponseData {
-  id: string; // 제품 ID
-  title: string; // 제품 이름
-  price: number; // 제품 가격
-  description: string; // 제품 상세 설명
-  tags: string[]; // 제품 태그
-  thumbnail: string; // 제품 썸네일 이미지(URL)
-  photo: string; // 제품 상세 이미지(URL)
-  isSoldOut: boolean; // 제품 매진 여부
-  reservations: Reservation[]; // 제품의 모든 예약 정보 목록
-  discountRate: number; // 제품 할인율
+// 예약 날짜
+interface SelectedDate {
+  date: string;
 }
 
-// 예약 정보가 있는 경우
-interface Reservation {
-  start: string; // 예약 시작 시간
-  end: string; // 예약 종료 시간
-  isCanceled: boolean; // 예약 취소 여부
-  isExpired: boolean; // 예약 만료 여부
+// 예약 시간
+interface SelectedTime {
+  start: string;
+  end: string;
+  timeDiffer: string;
+}
+
+// 예약 인원
+interface SelectedGuests {
+  guests: number;
 }
 
 // ProductDetail 컴포넌트
@@ -57,6 +61,45 @@ const ProductDetail: React.FC = () => {
 
   // 제품의 tags를 가져와 해시태그 기호를 붙여 출력
   const hashTags = product.tags.map((tag) => `#${tag}`);
+
+  // antd DatePicker - 예약 날짜
+  const onChangeDate: DatePickerProps['onChange'] = (date, dateString) => {
+    const isoDate = date?.toISOString();
+    dispatch(selectedDate(dateString as any));
+    console.log('iso형식은', isoDate); // heropy와 동일함
+    console.log('dateString은', dateString); // 2023-06-21
+  };
+
+  // antd TimePicker - 예약 시간
+  const onChangeTime = (
+    values: [Dayjs | null, Dayjs | null],
+    timeString: [string, string]
+  ) => {
+    // 선택한 입실 시간을 toISOString() 함수로 출력할 경우 UTC 기준 시간으로 반환함
+    // antd에서 사용하고 있는 dayjs의 add() 메서드로 9시간을 추가하여
+    // 대한민국 서울 기준(UTC+9) 시간으로 toISOString() 함수 적용
+    const isoStartTimeString = values[0]?.add(9, 'hour').toISOString();
+    const isoEndTimeString = values[1]?.add(9, 'hour').toISOString();
+
+    // dayjs의 diff() 메서드로 선택한 입실 시간과 퇴실 시간의 시간차를 반환하지만
+    // JavaScript의 Date 객체는 milliseconds 단위로 시간을 저장하기 때문에,
+    // diff() 메서드의 결과값이 밀리초 단위 때문에 정확하게 나오지 않을 수 있음
+    // 1시간을 밀리초로 변환하는 값으로 나누고, toFixed() 메서드로 소수점 자르기
+    // 1,000ms(=1초) * 60s(=1분) * 60m(=1시간) => 1시간을 ms로 변환
+    const timeOfUse = (
+      (values[1]?.diff(values[0]) as number) /
+      (1000 * 60 * 60)
+    ).toFixed(0);
+
+    dispatch(selectedTime(timeString as any));
+    dispatch(selectedTime(timeOfUse as any));
+  };
+
+  // antd InputNumber - 예약 인원
+  const onChangePerson = (value: 1 | 50 | null) => {
+    dispatch(selectedGuests(value as any));
+    console.log('예약 인원은? ', value);
+  };
 
   // 제품의 description을 가져와 문자열에 포함된 <br>을 기준으로 나누고 빈 문자열만 제거
   const productInfo = (product.description || '')
@@ -78,25 +121,14 @@ const ProductDetail: React.FC = () => {
   );
   const facilityInfo = productInfo.slice(facilityInfoIndex + 1);
 
-  // // antd InputNumber - 예약 인원
-  // const selectBefore = (
-  //   <Select defaultValue="minus" style={{ width: 60 }}>
-  //     <Option value="minus">-</Option>
-  //   </Select>
-  // );
-  // const selectAfter = (
-  //   <Select defaultValue="add" style={{ width: 60 }}>
-  //     <Option value="add">+</Option>
-  //   </Select>
-  // );
-
   // antd Modal - 예약하기 버튼
-  const showModal = () => {
+  const showLoginModal = () => {
     setIsModalOpen(true);
   };
 
-  const handleOk = () => {
-    navigate(`/signin`);
+  // 예약하기 버튼 클릭 시 비로그인 상태라면 로그인이 필요함을 안내하고 로그인 페이지로 이동
+  const handleOk = async () => {
+    await navigate('/signin');
   };
 
   const handleCancel = () => {
@@ -104,7 +136,7 @@ const ProductDetail: React.FC = () => {
   };
 
   const handleProductPayment = () => {
-    accessToken ? navigate(`/productpayment`) : showModal();
+    accessToken ? navigate('/productpayment') : showLoginModal();
   };
 
   useEffect(() => {
@@ -121,6 +153,7 @@ const ProductDetail: React.FC = () => {
 
   return (
     <main className={styles.inner}>
+      <h1>{}</h1>
       <Carousel autoplay>
         <div>
           <img
@@ -191,22 +224,37 @@ const ProductDetail: React.FC = () => {
             <p>· 예약 날짜 : </p>
             <ConfigProvider locale={locale}>
               <DatePicker
+                size={'large'}
                 placement={'bottomLeft'}
                 placeholder={'날짜를 선택해주세요'}
-                style={{ width: '52%', margin: '5px 0' }}
+                onChange={onChangeDate}
+                style={{ width: '55%', margin: '5px 0' }}
               />
             </ConfigProvider>
             <br />
             <p>· 예약 시간 : </p>
             <TimePicker.RangePicker
+              size="large"
               use12Hours
-              format="h:mm a"
+              format="hh a"
               placeholder={['입실 시간', '퇴실 시간']}
-              style={{ width: '52%', marginBottom: '5px' }}
+              onChange={
+                onChangeTime as (values: unknown, timeString: unknown) => void
+              }
+              style={{ width: '55%', marginBottom: '5px' }}
             />
             <br />
             <p>· 예약 인원 : </p>
-            <Space direction="vertical" style={{ width: '52%' }}></Space>
+            <Space direction="vertical" style={{ width: '52%' }}>
+              <InputNumber
+                size="large"
+                min={1}
+                max={50}
+                defaultValue={1}
+                onChange={onChangePerson}
+              />
+              <p>(최소 1명, 최대 50명)</p>
+            </Space>
           </form>
         </Card.Grid>
 
